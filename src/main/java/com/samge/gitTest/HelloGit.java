81fafb71ad1f2b8b78c065e57918d74c7d9e260b
samgeloo <samge520@hotmail.com>
2022-06-21 23:11:14 +0800

    第一次修改

--- src/main/java/com/samge/gitTest/HelloGit.java
+++ src/main/java/com/samge/gitTest/HelloGit.java
@@ -10,6 +10,7 @@ package com.samge.gitTest;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("新建");
+        System.out.println("第一次修改");
     }
 
 }
