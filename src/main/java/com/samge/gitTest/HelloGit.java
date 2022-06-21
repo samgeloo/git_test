35c7890c2d084b92766a4b61ddc3d99810080d10
samgeloo <samge520@hotmail.com>
2022-06-21 23:09:22 +0800

    新建

---
+++ src/main/java/com/samge/gitTest/HelloGit.java
@@ -0,0 +1,15 @@
+package com.samge.gitTest;
+
+/**
+ * @author ：samgeloo
+ * @version ：v1.0.0
+ * @Package : com.samge.gitTest
+ * @Description :
+ * @date : 2022-06-21 22:46
+ **/
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("新建");
+    }
+
+}
